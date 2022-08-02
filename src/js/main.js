0115d48351c3121c8a8fa2f4da8f4e14ce77e703
Anderson Vieira <asvieira.br@gmail.com>
2022-08-02 19:14:07 -0300

    Aplicando estilo: configurações gerais

--- src/js/main.js
+++ src/js/main.js
@@ -20,19 +20,21 @@ async function getAllProducts() {
     let divProductData = document.createElement("div")
     divProductData.className = "product-data"
     
-    let productName = document.createElement("h3");
-    let productOldPrice = document.createElement("h4");
-    let productPrice = document.createElement("h2");
+    let productName = document.createElement("h5");
+    let productOldPrice = document.createElement("p");
+    let productPrice = document.createElement("h4");
     let productInstallments = document.createElement("p")
     let productLink = document.createElement("a");
+
+    let productCurrency = "R$"
 
     productImage = new Image()
     productImage.src = product.image
 
     productName.innerText = product.name;
-    productOldPrice.innerText = `De: ${product.oldPrice}`;
-    productPrice.innerText = `Por: ${product.price}`;
-    productInstallments.innerText = `Ou ${product.installments.count}x de ${product.installments.value}`
+    productOldPrice.innerText = `De: ${productCurrency} ${(product.oldPrice).toFixed(2)}`;
+    productPrice.innerText = `Por: ${productCurrency} ${(product.price).toFixed(2)}`;
+    productInstallments.innerText = `Ou ${product.installments.count}x de ${productCurrency} ${(product.installments.value).toFixed(2)}`
     productInstallmentsValue = product.installments.value
     productLink.innerText = "Comprar"
     productLink.setAttribute("href", `/product.html?id=${product.id}`);
