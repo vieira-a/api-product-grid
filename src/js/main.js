09859e1d52c550c555aacb1c2896162d69608fc8
Anderson Vieira <asvieira.br@gmail.com>
2022-08-02 17:30:01 -0300

    Criando estrutura de tabela para receber os dados dos produtos: imagem e nome

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products"
 
 const productsContainer = document.querySelector("#products-container");
-//console.log(productsContainer)
-//Get all products
 
 async function getAllProducts() {
   const response = await fetch(url)
@@ -13,59 +11,76 @@ async function getAllProducts() {
   
   data = data.products
   
-  
   data.map((product) => {
-    //console.log(product.installments)
     
-    let divProductImage = document.createElement("div")
-    divProductImage.className = "product-image"
-    
-    let divProductContent = document.createElement("div");
-    divProductContent.className = "product"
+    //Construção da tabela global
+    let productTable = document.createElement("table")
+    let productThead = document.createElement("thead")
 
-    let divProductData = document.createElement("div")
-    divProductData.className = "product-data"
-    
+    //Variaveis da linha 1
+    let productTr1 = document.createElement("tr")
+    //Linha da imagem
+    let productTdImage = document.createElement("td")
+    productTdImage.rowSpan = "4"
+    //Linha do nome
+    let productTdName = document.createElement("td")
+
+    let productImage = new Image();
+    productImage.src = product.image;
+
     let productName = document.createElement("h3");
-    let productOldPrice = document.createElement("h4");
-    let productPrice = document.createElement("h2");
-    let productInstallments = document.createElement("p")
-    let productLink = document.createElement("a");
-
-    productImage = new Image()
-    productImage.src = product.image
-
-
     productName.innerText = product.name;
-    productOldPrice.innerText = product.oldPrice;
-    productPrice.innerText = product.price;
-    productInstallmentsCount = product.installments.count
-    productInstallmentsValue = product.installments.value
-    productLink.innerText = "Comprar"
-    productLink.setAttribute("href", `/product.html?id=${product.id}`);
+
+    //Inserindo elementos da linha 1
+    //Imagem dentro da linha
+    productTdImage.append(productImage)
+    //Nome dentro da linha
+    productTdName.append(productName)
+    //Carregando a thead
+    productThead.append(productTdImage)
+    productThead.append(productTdName)
+    //carregando a table
+    productTable.append(productThead)
     
-    divProductImage.append(productImage);
+    //Carregamento global da div principal
+    productsContainer.append(productTable)
 
-    divProductContent.append(divProductImage);
+    // let divProductImage = document.createElement("div")
+    // divProductImage.className = "product-image"
+    
+    // let divProductContent = document.createElement("div");
+    // divProductContent.className = "product"
 
-    // divProductContent.append(productName);
-    // divProductContent.append(productStrDe);
-    // divProductContent.append(productOldPrice)
-    // divProductContent.append(productPrice);
-    // divProductContent.append(`Ou ${productInstallmentsCount}x de `);
-    // divProductContent.append(`R$ ${productInstallmentsValue}`);
-    // divProductContent.append(productLink);
-    // productsContainer.appendChild(divProductContent)
+    // let divProductData = document.createElement("div")
+    // divProductData.className = "product-data"
+    
+    // let productOldPrice = document.createElement("h4");
+    // let productPrice = document.createElement("h2");
+    // let productInstallments = document.createElement("p")
+    // let productLink = document.createElement("a");
 
-    divProductData.append(productName);
-    divProductData.append(productOldPrice)
-    divProductData.append(productPrice);
-    divProductData.append(`Ou ${productInstallmentsCount}x de `);
-    divProductData.append(`R$ ${productInstallmentsValue}`);
-    divProductData.append(productLink);
+    
 
-    productsContainer.appendChild(divProductImage)
-    productsContainer.appendChild(divProductData)
+    // productOldPrice.innerText = product.oldPrice;
+    // productPrice.innerText = product.price;
+    // productInstallmentsCount = product.installments.count
+    // productInstallmentsValue = product.installments.value
+    // productLink.innerText = "Comprar"
+    // productLink.setAttribute("href", `/product.html?id=${product.id}`);
+    
+    // divProductImage.append(productImage);
+
+    // divProductContent.append(divProductImage);
+
+    // divProductData.append(productName);
+    // divProductData.append(productOldPrice)
+    // divProductData.append(productPrice);
+    // divProductData.append(`Ou ${productInstallmentsCount}x de `);
+    // divProductData.append(`R$ ${productInstallmentsValue}`);
+    // divProductData.append(productLink);
+
+    // productsContainer.appendChild(divProductImage)
+    // productsContainer.appendChild(divProductData)
   })
 
 }
