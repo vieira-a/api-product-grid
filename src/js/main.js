e05f148ed01acfe166488ed59d6cdce9fd12a4dd
Anderson Vieira <asvieira.br@gmail.com>
2022-08-02 18:31:24 -0300

    Todos os dados dos produtos carregados de forma organizada em divs para facilitar aplicação de grid e flexbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products"
 
 const productsContainer = document.querySelector("#products-container");
+//console.log(productsContainer)
+//Get all products
 
 async function getAllProducts() {
   const response = await fetch(url)
@@ -11,76 +13,60 @@ async function getAllProducts() {
   
   data = data.products
   
+  
   data.map((product) => {
+    //console.log(product.installments)
     
-    //Construção da tabela global
-    let productTable = document.createElement("table")
-    let productThead = document.createElement("thead")
+    let divProductImage = document.createElement("div")
+    divProductImage.className = "product-image"
+    
+    let divProductContent = document.createElement("div");
+    divProductContent.className = "product"
 
-    //Variaveis da linha 1
-    let productTr1 = document.createElement("tr")
-    //Linha da imagem
-    let productTdImage = document.createElement("td")
-    productTdImage.rowSpan = "4"
-    //Linha do nome
-    let productTdName = document.createElement("td")
-
-    let productImage = new Image();
-    productImage.src = product.image;
-
+    let divProductData = document.createElement("div")
+    divProductData.className = "product-data"
+    
     let productName = document.createElement("h3");
+    let productOldPrice = document.createElement("h4");
+    let productPrice = document.createElement("h2");
+    let productInstallments = document.createElement("p")
+    let productLink = document.createElement("a");
+
+    productImage = new Image()
+    productImage.src = product.image
+
+
     productName.innerText = product.name;
-
-    //Inserindo elementos da linha 1
-    //Imagem dentro da linha
-    productTdImage.append(productImage)
-    //Nome dentro da linha
-    productTdName.append(productName)
-    //Carregando a thead
-    productThead.append(productTdImage)
-    productThead.append(productTdName)
-    //carregando a table
-    productTable.append(productThead)
+    productOldPrice.innerText = `De: ${product.oldPrice}`;
+    productPrice.innerText = `Por: ${product.price}`;
+    productInstallments.innerText = `Ou ${product.installments.count}x de ${product.installments.value}`
+    productInstallmentsValue = product.installments.value
+    productLink.innerText = "Comprar"
+    productLink.setAttribute("href", `/product.html?id=${product.id}`);
     
-    //Carregamento global da div principal
-    productsContainer.append(productTable)
+    divProductImage.append(productImage);
 
-    // let divProductImage = document.createElement("div")
-    // divProductImage.className = "product-image"
-    
-    // let divProductContent = document.createElement("div");
-    // divProductContent.className = "product"
+    divProductContent.append(divProductImage);
 
-    // let divProductData = document.createElement("div")
-    // divProductData.className = "product-data"
-    
-    // let productOldPrice = document.createElement("h4");
-    // let productPrice = document.createElement("h2");
-    // let productInstallments = document.createElement("p")
-    // let productLink = document.createElement("a");
+    // divProductContent.append(productName);
+    // divProductContent.append(productStrDe);
+    // divProductContent.append(productOldPrice)
+    // divProductContent.append(productPrice);
+    // divProductContent.append(`Ou ${productInstallmentsCount}x de `);
+    // divProductContent.append(`R$ ${productInstallmentsValue}`);
+    // divProductContent.append(productLink);
+    // productsContainer.appendChild(divProductContent)
 
-    
+    divProductData.append(productName);
+    divProductData.append(productOldPrice)
+    divProductData.append(productPrice);
+    divProductData.append(productInstallments)
+    //divProductData.append(productInstallmentsCount);
+    //divProductData.append(productInstallmentsValue);
+    divProductData.append(productLink);
 
-    // productOldPrice.innerText = product.oldPrice;
-    // productPrice.innerText = product.price;
-    // productInstallmentsCount = product.installments.count
-    // productInstallmentsValue = product.installments.value
-    // productLink.innerText = "Comprar"
-    // productLink.setAttribute("href", `/product.html?id=${product.id}`);
-    
-    // divProductImage.append(productImage);
-
-    // divProductContent.append(divProductImage);
-
-    // divProductData.append(productName);
-    // divProductData.append(productOldPrice)
-    // divProductData.append(productPrice);
-    // divProductData.append(`Ou ${productInstallmentsCount}x de `);
-    // divProductData.append(`R$ ${productInstallmentsValue}`);
-    // divProductData.append(productLink);
-
-    // productsContainer.appendChild(divProductImage)
-    // productsContainer.appendChild(divProductData)
+    productsContainer.appendChild(divProductImage)
+    productsContainer.appendChild(divProductData)
   })
 
 }
