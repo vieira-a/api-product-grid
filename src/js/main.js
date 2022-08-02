0b2dbe01f871e436cac2569425818fad5fbf2b93
Anderson Vieira <asvieira.br@gmail.com>
2022-08-02 18:32:41 -0300

    Todos os dados dos produtos carregados de forma organizada em divs para facilitar aplicação de grid e flexbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,16 @@
 const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products"
 
 const productsContainer = document.querySelector("#products-container");
-//console.log(productsContainer)
-//Get all products
 
 async function getAllProducts() {
   const response = await fetch(url)
-
-  //console.log(response)
 
   let data = await response.json()
   
   data = data.products
   
-  
   data.map((product) => {
-    //console.log(product.installments)
-    
+        
     let divProductImage = document.createElement("div")
     divProductImage.className = "product-image"
     
@@ -35,7 +29,6 @@ async function getAllProducts() {
     productImage = new Image()
     productImage.src = product.image
 
-
     productName.innerText = product.name;
     productOldPrice.innerText = `De: ${product.oldPrice}`;
     productPrice.innerText = `Por: ${product.price}`;
@@ -48,27 +41,15 @@ async function getAllProducts() {
 
     divProductContent.append(divProductImage);
 
-    // divProductContent.append(productName);
-    // divProductContent.append(productStrDe);
-    // divProductContent.append(productOldPrice)
-    // divProductContent.append(productPrice);
-    // divProductContent.append(`Ou ${productInstallmentsCount}x de `);
-    // divProductContent.append(`R$ ${productInstallmentsValue}`);
-    // divProductContent.append(productLink);
-    // productsContainer.appendChild(divProductContent)
-
     divProductData.append(productName);
     divProductData.append(productOldPrice)
     divProductData.append(productPrice);
     divProductData.append(productInstallments)
-    //divProductData.append(productInstallmentsCount);
-    //divProductData.append(productInstallmentsValue);
     divProductData.append(productLink);
 
     productsContainer.appendChild(divProductImage)
     productsContainer.appendChild(divProductData)
   })
-
 }
 
 getAllProducts()
